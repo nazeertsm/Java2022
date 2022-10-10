62e6b5a33914d7c6dcbba5684be350b375563dcf
ahmedna <nazeer.ahmed@navis.com>
2022-10-10 07:03:21 +0530

    updated

--- Learncode/src/InterThreadCommunicationApp/in/ineuron/main/Test.java
+++ Learncode/src/InterThreadCommunicationApp/in/ineuron/main/Test.java
@@ -1,11 +1,11 @@
 package InterThreadCommunicationApp.in.ineuron.main;
 
 class Demo extends Thread {
-//data is updated
+	//data is updated
 	int total = 0;
 
 	public void run() {
-//producer thread
+		//producer thread
 		// sum of first 100 numbers
 		for (int i = 1; i <= 100; i++) {
 			total = total + i;
